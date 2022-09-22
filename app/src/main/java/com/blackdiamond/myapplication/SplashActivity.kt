9b5b5c80c22654141322b4f1a3c1c248784a594d
DyaaEDeen <dyaaeldeen057@gmail.com>
2022-09-22 13:34:09 +0200

    Added Runtime permission check

--- app/src/main/java/com/blackdiamond/myapplication/SplashActivity.kt
+++ app/src/main/java/com/blackdiamond/myapplication/SplashActivity.kt
@@ -1,11 +1,65 @@
 package com.blackdiamond.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.Handler
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class SplashActivity : AppCompatActivity() {
+
+    private var splashTimeOut = 999
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed(
+            {
+                if (!checkSelfPermission()) {
+                    requestPermission()
+                } else {
+                    loadImages()
+                }
+            }, splashTimeOut.toLong()
+        )
     }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 9999
+        )
+    }
+
+    private fun checkSelfPermission(): Boolean {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            return false
+        }
+        return true
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if(requestCode == 9999){
+            if(grantResults.isNotEmpty()){
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    loadImages()
+                }else{
+                    Toast.makeText(this,"Permission was denied, cant load the images!",Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    private fun loadImages() {
+        Toast.makeText(this,"I have Permission !",Toast.LENGTH_SHORT).show()
+    }
+
 }
